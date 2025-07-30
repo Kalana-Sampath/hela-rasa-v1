ac88dd491b2ac0945a85c88987604fe020151b21
Kalana <kalanas414@gmail.com>
2025-07-30 08:47:00 +0530

    frontend setup with folder and file structure

---
+++ mobile/app/_layout.jsx
@@ -0,0 +1,12 @@
+import { Slot } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
+export default function RootLayoutNav() {
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      
+      <Slot/>
+    </ClerkProvider>
+  );
+}
